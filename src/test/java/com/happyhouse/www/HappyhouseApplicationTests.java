8417729805c19b117e46612434f8b6281a79cd8c
slovecoms <gloveomg@gmail.com>
2023-11-04 23:51:37 +0900

    init commit

---
+++ src/test/java/com/happyhouse/www/HappyhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyhouse.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
